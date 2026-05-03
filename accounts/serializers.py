ee89b130d16f16436dca0ed2fc38fff03eff9300
KodewithArun <contactwitharunlaudari@gmail.com>
2026-05-03 11:42:56 +0545

    JWT authentication with custom login response

---
+++ accounts/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+class CustomTokenSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        user = self.user
+
+        data['user'] = {
+            'id': user.id,
+            'email': user.email,
+            'role': user.role,
+        }
+
+        return data
--- config/settings.py
+++ config/settings.py
@@ -122,3 +122,9 @@ STATIC_URL = 'static/'
 
 
 AUTH_USER_MODEL = 'accounts.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
